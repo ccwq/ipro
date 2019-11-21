74fa72cd076217251644dea6ca309c19e5bda798
ccwq <scwqd@qq.com>
2019-11-21 17:30:10 +0800

    一堆bug解决

--- src/string/StringUtils.js
+++ src/string/StringUtils.js
@@ -1,3 +1,5 @@
+const randomDic = new Map();
+
 export default class StringUtils {
     /**
      * 使用字符串的第一个字母大写
@@ -22,4 +24,18 @@ export default class StringUtils {
         let [first, ...rest] = string;
         return first.toUpperCase() + rest.join('');
     } ;
+
+
+    /**
+     * 产生一个不重复的字符串
+     * @returns {string}
+     */
+    static random(){
+        let ret =  Math.random().toString(32).substr(2);
+        if(randomDic.get(ret)){
+            return StringUtils.random();
+        }
+        randomDic.set(ret, true);
+        return ret;
+    }
 }
