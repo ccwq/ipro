db1572c5789ba4741698f4c9b485bbe7a011385c
ccwq <scwqd@qq.com>
2020-10-21 15:40:34 +0800

    BPromise增加resolve,和reject方法

--- src/promise/BPromise.js
+++ src/promise/BPromise.js
@@ -2,6 +2,17 @@
  * 异形Promise，暴露resolve,resolve在外的Promise
  */
 export default class BPromise extends Promise{
+    static resolve(value){
+        let p = new BPromise;
+        p._resolve(value);
+        return  p;
+    }
+
+    static reject(value){
+        let p = new BPromise;
+        p._reject(value);
+        return  p;
+    }
     constructor(callback) {
         let _resolve, _reject;
         super((resolve, reject)=>{
