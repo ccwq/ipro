6204952c1318aac7b13cd9800a81b733dc01e96b
ccwq <scwqd@qq.com>
2021-03-26 11:37:12 +0800

    增加日期工具,all2date,转换各种形式的日期到date类型

--- src/date/DateUtils.js
+++ src/date/DateUtils.js
@@ -1,45 +1,104 @@
 import $D from "date-prototype";
+
+/*
+ * 转换各种类型的数据到日期
+ * @param input {Date|Number|String}
+ * @returns {Date|*}
+ */
+export const all2date = function(input){
+    const now = new Date();
+
+    //空对象
+    if (!input) {
+        return new Date();
+
+    //日期对象
+    }else if (input instanceof Date) {
+        return input;
+
+    //数字
+    }else if (typeof input == "number") {
+
+        const str = (input + "");
+
+        const strArr = str.split("");
+        const _year = strArr.splice(0, 4).join("");
+        const _month = parseInt(strArr.splice(0, 2).join("")) - 1;
+        const _day = strArr.splice(0, 2).join("");
+
+        //年份
+        if (str.length == 4) {
+            now.setFullYear(_year);
+            return now;
+        //年月
+        }else if (str.length == 6) {
+            now.setFullYear(_year);
+            now.setMonth(_month);
+            return now;
+
+        //年月日
+        }else if (str.length == 8) {
+            now.setFullYear(_year);
+            now.setMonth(_month);
+            now.setDate(_day);
+            return now;
+        }else{
+            return new Date(input);
+        }
+
+        //字符串时间戳
+    }else if (typeof input == "string") {
+        input = input.trim();
+
+        //纯数字
+        if (/^\d+$/.test(input)) {
+            return all2date(parseInt(input));
+
+        //带有其他字符串
+        }else{
+
+            //日期分割
+            const parts = input.split(/[-:\sTZ\+年月日时分秒]/);
+            const [
+                y  = now.getFullYear(),
+                m  = now.getMonth() + 1,
+                d  = mow.getDate(),
+                h  = now.getHours(),
+                mm = now.getMinutes(),
+                s  = now.getSeconds()
+            ] = parts;
+
+            const num = parseInt([
+                y,
+                (m + "").padStart(2, 0),
+                (d + "").padStart(2, 0),
+            ].join(""));
+
+            //只有年月日的情况
+            if (parts.length <= 3) {
+                return all2date(num)
+
+            //包含时间
+            }else{
+                const date = all2date(num);
+                date.setHours(h, mm, s);
+                return date;
+            }
+        }
+    }
+}
+
+export const parse2date = all2date;
+
 export default class DateUtils{
 
     /*
-     * 转换各种类型的数据到天气
-     * @param input number
+     * 转换各种类型的数据到日期
+     * @param input {Date|Number|String}
      * @returns {Date|*}
      */
     static parse2date(input) {
-
-        //数字或者字符串时间戳
-        if (typeof input == "number" || /^\d+$/.test(input + "")) {
-
-            //4标识是年
-            if(input.length === 4){
-                let a = new Date;
-                a.setFullYear(input * 1);
-                return a;
-
-            //其他情况，自求多福
-            }else{
-                return new Date(input * 1);
-            }
-
-            //空对象
-        } else if (!input) {
-            return new Date();
-
-            //日期对象
-        }else if (input.constructor == Date) {
-            return input;
-        }
-        //日期字符串
-        var parts = input.split(/[-:\sTZ\+年月日时分秒]/);
-
-
-        //只有年月的情况2018-06
-        if (parts.length == 2) {
-            parts[2] = 1;
-        }
-
-        return new Date(parts[0], parts[1] - 1, parts[2], parts[3] || 0, parts[4] || 0, parts[5] || 0);
+        return all2date(input);
     }
 
 
